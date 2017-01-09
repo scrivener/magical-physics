d5f51234cab9a700f646924e57819976992daa6e
Adam Lerner <eletari@gmail.com>
2017-01-08 18:48:40 -0800

    Yellow crystals repel each other.

---
+++ Assets/YellowThing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowThing : MonoBehaviour {
+
+	public float yellowRepelFactor = 500.0f;
+	private Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		repelNearbyYellowThings();
+	}
+
+	void repelNearbyYellowThings() {
+		Vector2 push = new Vector2();
+		GameObject[] yellowThings = GameObject.FindGameObjectsWithTag("Yellow");
+		foreach (GameObject crystal in yellowThings) {
+			if (crystal == gameObject) {
+				continue;
+			}
+			Vector2 directionToCrystal = crystal.transform.position - transform.position;
+			float distanceToCrystal = directionToCrystal.magnitude;
+			float inverseDistanceSquare = 1 / (distanceToCrystal * distanceToCrystal);
+			push += directionToCrystal.normalized * yellowRepelFactor * inverseDistanceSquare;
+		}
+		rigidbody2D.AddForce(-push);
+	}
+}
